889621a6d21a654ee503c012f067e4171330c5b0
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2025-01-06 22:15:45 -0300

    Adiciona primeiro exercício resolvido de módulo de React, com conteúdo simples de um documento xml em react

---
+++ ex-01/README.md
@@ -0,0 +1,13 @@
+# Exercício 1
+Crie um novo projeto React usando o Vite, remova todo o conteúdo inicial do projeto e então reproduza a página html abaixo usando JSX.
+
+**Imagem utilizada no exercício:**
+
+<img src="https://onebitcode.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fea7d49f7-85ff-4dc5-96bf-7101fd3e1565%2Freact.png?table=block&id=8bc24587-6336-4b7b-9fe0-1a5d63bccbc8&spaceId=6e5271d8-2f68-42f5-aa75-5978bbff47fa&width=270&userId=&cache=v2">
+
+Lembrando que você pode utilizá-la colocando-a na pasta “public” do Vite
+(usando o caminho “/nome-da-imagem.png”)
+
+**Página HTML a ser reproduzida:**
+
+<img src="https://onebitcode.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fb90fa068-643a-40a0-9f2d-79b8fca2e22a%2Fscreenshot-exercicio-1.png?table=block&id=b8e84d8c-96ec-4eca-bb38-c64e8e39c4d2&spaceId=6e5271d8-2f68-42f5-aa75-5978bbff47fa&width=1420&userId=&cache=v2">
---
+++ ex-01/react/src/App.jsx
@@ -0,0 +1,34 @@
+export default function App(){
+  return (
+    <div>
+      <img src="../../public/react.webp" alt="" />
+      
+      <h1>React</h1>
+      <p>A biblioteca para interfaces de usuário web nativas</p>
+      <button>Aprenda React</button>
+      <button>Referência da API</button>
+      
+      <hr />
+      
+      <h2>Crie interfaces de usuário de componentes</h2>
+      <p>React permite que você construa interfaces de usuário a partir de pedaços individuais chamados componentes</p>
+      
+      <hr />
+
+      <h2>Escreva componentes com código de marcação</h2>
+      <p>Componentes React são funções JavaScript. A sintaxe de marcação é chamada JSX. É uma extensão da sintaxe JavaScript popularizada pelo React.</p>
+
+      <hr />
+
+      <h2>Próximos passos</h2>
+      <ul>
+        <li>Uso de dados dinâmicos no JSX</li>
+        <li>Criação de novos componentes</li>
+        <li>Estilização de componentes</li>
+        <li>Reutilização de componentes</li>
+        <li>Uso de props e children</li>
+        <li>Uso de eventos do JavaScript</li>
+      </ul>
+    </div>
+  )
+}
