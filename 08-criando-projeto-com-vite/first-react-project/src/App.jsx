8f5c0a8bc2c049b032cca787486ebb606311a01f
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2025-01-06 21:51:26 -0300

    Adiciona conteúdo de aula 8 do módulo de React: Criando projeto React com Vite

---
+++ 08-criando-projeto-com-vite/first-react-project/src/App.jsx
@@ -0,0 +1,16 @@
+export default function App(){
+  return (
+    <div>
+      <h1>Olá, Mundo!</h1>
+      <p>Este é o meu primeiro projeto usando React e Vit</p>
+      <p>Próximos passos:</p>
+      <ul>
+        <li>Praticar com exercícios</li>
+        <li>Aprender a usar dados dinâmicos</li>
+        <li>Criar novos componentes</li>
+        <li>Estilizar os componentes</li>
+        <li>Reutilizar os componentes</li>
+      </ul>
+    </div>
+  )
+}
