32c703b329e8f44ee05d2193f089642510c0c5cc
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2025-01-07 21:06:10 -0300

    Adiciona conteúdo de aula 13 do módulo de React em projeto reutilizável

--- projetos-reutilizáveis/react-fundamentals/src/App.jsx
+++ projetos-reutilizáveis/react-fundamentals/src/App.jsx
@@ -1,14 +1,14 @@
+import StatusText from "./components/StatusText"
+import Subtitle from "./components/Subtitle"
 import Title from "./components/Title"
-import { Subtitle } from "./components/Subtitle"
-import { StatusText } from "./components/StatusText"
-
+import styles from "./App.module.css"
 
 export default function App() {
   return (
-    <div>
-      <Title/>
-      <Subtitle/>
-      <StatusText/>
+    <div className={styles.app}>
+      <Title />
+      <Subtitle />
+      <StatusText />
     </div>
   )
 }
--- projetos-reutilizáveis/react-fundamentals/src/components/Subtitle.jsx
+++ projetos-reutilizáveis/react-fundamentals/src/components/Subtitle.jsx
@@ -1,1 +1,6 @@
-export const Subtitle = () => <h2>It´s easy. Like 1 + 1 is {1 + 1}</h2>
+const Subtitle = () => (
+    // eslint-disable-next-line react/no-unescaped-entities
+    <h2 style={{ color: "#e5e5e5" }}>It's easy, like 1 + 1 is {1 + 1}</h2>
+  )
+  
+  export default Subtitle
